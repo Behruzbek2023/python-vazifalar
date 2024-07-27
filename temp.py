5e9cc88bffecfba132471d1e45046874b0d4c67c
Behruzbek <123858278+Behruzbek2023@users.noreply.github.com>
2024-07-26 21:23:33 -0700

    ghghfg
    fgghfg

---
+++ temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#soyt = ['bmw','audi']
+Ism = str(input('Isminggizni kiriting'))
+ism = 'uzun ekan'
+if len(Ism)<= 8 :
+    print('ismingiz yaxshi')
+else: 
+    print(ism.title())    
